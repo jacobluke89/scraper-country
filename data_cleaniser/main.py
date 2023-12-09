99014526c2be5d6c88a7ff60ba6d8e0d6d256e33
Jacob Bickerstaff <jacob.bickerstaff@intellectai.co.uk>
2023-12-09 20:25:11 +0000

    renamed.

--- data_cleaniser/main.py
+++
@@ -1,6 +0,0 @@
-from databaseManager.connection import DBManager
-
-def run():
-    dbc = DBManager('postgres', 'jacobbickerstaff', 'postgres', 'localhost', 5432)
-    cur = dbc.get_cursor()
-    print('success!')
