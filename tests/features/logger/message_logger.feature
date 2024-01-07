7202f0b6a80d9fc3308ea8c0eede72306c683954
Jacob Bickerstaff <jacoblukebickerstaff@gmail.com>
2024-01-07 17:28:37 +0000

    message logger tests created.

--- tests/features/logger/message_logger.feature
+++ tests/features/logger/message_logger.feature
@@ -1,7 +1,9 @@
+@setup_message_logger_database
+@teardown_message_logger_database
 Feature: Message logging System
 
     Scenario: Log a message from the calling function using message_logger
-    Given a function "callingFunction" that calls message_logger with "Special message"
-    When I call the calling function
-    Then a log entry with "MESSAGE LOGGER: Special message" should be saved in the database
-    And the log entry should indicate that it was called from "callingFunction"
+    Given a function 'message_call' that calls message_logger with 'Special message'
+    When I call the message function
+    Then a log entry with 'MESSAGE LOGGER: called by message_call message is Special message' should be saved in the database
+    And the log level should be message and function name is message_call
---
+++ tests/steps/message_logger_steps.py
@@ -0,0 +1,41 @@
+from behave import given, step, then
+from behave.runner import Context
+
+from logger.logger import message_logger, LogLevel
+from tests.environment import get_database_name
+from common import table_exists_check
+
+@given("a function '{calling_function}' that calls message_logger with '{special_message}'")
+def function_calls_message_logger(context: Context, calling_function: str, special_message: str):
+    def message_call():
+        message_logger(f"called by {calling_function} message is {special_message}",
+                       message_level=LogLevel.MESSAGE, db_manager=context.db_manager)
+    message_call()
+    context.function = message_call
+
+@then("a log entry with '{message}' should be saved in the database")
+def check_message_log(context: Context, message: str):
+    db_conn = context.db_manager
+    db_name = get_database_name(context)
+
+    cursor, table_exists = table_exists_check(db_conn, db_name, 'logger')
+    entry_exists: bool = False
+    try:
+        if table_exists:
+            query = f"""
+            SELECT EXISTS(
+                SELECT * 
+                FROM {db_name}.logger
+                WHERE info = %s            
+            );
+            """
+            cursor.execute(query, (message,))
+            entry_exists = cursor.fetchone()[0]
+    except Exception as e:
+        raise AssertionError(F"Database error occurred: {e}")
+    assert entry_exists is True, f"{message} does not exist in {db_name}"
+
+
+@step("the log entry should indicate that it was called from '{function}'")
+def check_log_entry_call(context: Context, function: str):
+    pass
